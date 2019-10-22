a43b098e9476e06097bf726452d09472818107bc
Archiekins <archiekins@outlook.com>
2019-10-22 21:42:16 +0100

    Add intiial project

---
+++ src/main/java/com/tat/App.java
@@ -0,0 +1,13 @@
+package com.tat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
